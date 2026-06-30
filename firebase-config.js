4b01d045400af408f6caa10d0b0f8fe5fdd1cff3
tsvalencio-IA <tsvalencio@gmail.com>
2026-06-30 16:45:31 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  databaseURL: "https://COLE_AQUI-default-rtdb.firebaseio.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.firebasestorage.app",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
-};
+window.firebaseConfig = {
+    apiKey: "AIzaSyBS7y325uyAn_rFIecITZdCvuoTqyE1I14",
+    authDomain: "thiago-e1362.firebaseapp.com",
+    databaseURL: "https://thiago-e1362-default-rtdb.firebaseio.com",
+    projectId: "thiago-e1362",
+    storageBucket: "thiago-e1362.firebasestorage.app",
+    messagingSenderId: "527506942270",
+    appId: "1:527506942270:web:8a390dd3abc7f7cda6246f"
+  };
